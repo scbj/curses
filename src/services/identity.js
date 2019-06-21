cfb58d0c91193d3e123bc9c1372bad0fe02f0f49
Sacha <contact@scbj.io>
2019-06-21 17:56:51 +0200

    Font (TT Commons) & Logo
    ◽ Import font
    ◽ Use real app logo
    ◽ Create base CSS

---
+++ src/services/identity.js
@@ -0,0 +1,29 @@
+import netlifyIdentity from 'netlify-identity-widget'
+
+netlifyIdentity.init({
+  APIUrl: 'https://curses.netlify.com/.netlify/identity',
+
+  // Allow Netlify to put its logo at the bottom of the login dialog
+  logo: true
+})
+
+window.identity = netlifyIdentity
+/**
+ * Returns true if the user is connected.
+ * @returns {Boolean}
+ */
+export function hasConnectedUser () {
+  return !!netlifyIdentity.currentUser()
+}
+
+/**
+ * Retreive the the JWT token of the current user or returns an empty string.
+ * @returns {String}
+ */
+export async function getBearerToken () {
+  return hasConnectedUser()
+    ? netlifyIdentity.currentUser().jwt()
+    : ''
+}
+
+export default netlifyIdentity
