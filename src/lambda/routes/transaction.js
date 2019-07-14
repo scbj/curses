fe60c6381769095f0fd6fa1eec7831343b406f5a
Sacha <contact@scbj.io>
2019-07-14 06:25:06 +0200

    Implement the create and list routes for transaction

--- src/lambda/routes/transaction.js
+++ src/lambda/routes/transaction.js
@@ -1,22 +1,41 @@
 import { authenticate } from './middlewares/auth'
+import Transaction from './models/Transaction'
+
+/**
+ * Returns the object given as parameter with only the desired attributes in the options.
+ * @param {*} params The original object
+ * @param {*} options The desired attributes
+ */
+function filterParams (params, options) {
+  const filtered = {}
+  for (const key in params) {
+    if (options.indexOf(key) > -1) {
+      filtered[key] = params[key]
+    }
+  }
+  return filtered
+}
 
 export default {
-  create (req, res) {
-    return res.json({
-      message: 'My happy msg ❤'
-    })
-  },
+  create: authenticate(async (req, res) => {
+    // Retreive user inputs
+    const expectedFields = [ 'description', 'amount', 'owner' ]
+    const params = filterParams(req.body, expectedFields)
+    params.date = new Date()
 
-  async list (req, res) {
-    await (function (ms) { return new Promise(resolve => setTimeout(resolve, ms)) })(2000)
-    return res.status(210).json({
-      message: 'Ohhhh foo'
-    })
-  },
+    // Create a Transaction from the specified params
+    const transaction = new Transaction(params)
 
-  bar: authenticate((req, res) => {
-    return res.json({
-      message: 'Vert'
-    })
+    // Save it to MongoDB
+    await transaction.save()
+    console.log(`🐣 Transaction created. { id: ${transaction._id}}`)
+
+    // Send the saved transaction
+    return res.status(201).json(transaction)
+  }),
+
+  list: authenticate(async (req, res) => {
+    const transactions = await Transaction.find()
+    return res.json(transactions)
   })
 }
