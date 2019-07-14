032b74bcc6acf785dd30f70d3be7482b76f51f81
Sacha <contact@scbj.io>
2019-07-14 19:05:14 +0200

    Add debug global variables

--- src/services/api.js
+++ src/services/api.js
@@ -20,6 +20,7 @@ export default async function api (action, payload) {
       body: payload && JSON.stringify(payload)
     }
     const response = await fetch('/.netlify/functions/api', options)
+    window.response = response
     return response.headers.get('Content-Type') === 'application/json'
       ? response.json()
       : response.text()
