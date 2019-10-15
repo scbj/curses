bb86a6d31f9935aea66193c2312bdb63fbf1c811
Sacha <contact@scbj.io>
2019-10-15 16:36:09 +0200

    Add JSDoc to capitalize filter

--- src/filters/string.js
+++ src/filters/string.js
@@ -1,3 +1,8 @@
+/**
+ * Capitalize the first letter of a string.
+ * @param {String} value
+ * @returns {String}
+ */
 export function capitalize (value) {
   if (value.length > 0) {
     return `${value[0].toUpperCase()}${value.substr(1)}`
